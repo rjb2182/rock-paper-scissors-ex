f8e4c5743440e7cd78aafec58582cb79f197dfb9
Rich Bubbico <rjb2182@stern.nyu.edu>
2019-06-05 19:50:21 -0400

    Winner outcome

--- game.py
+++ game.py
@@ -27,7 +27,51 @@ computer_choice = random.choice(options)
 print("___________")
 print("The Computer Chose:", computer_choice)
 
-#  Determine the winner
+#  DETERMINE THE WINNER
+
+# rock beats scissors
+
+# paper beats rock
+
+# scissors beats paper
+
+# same selection is a tie
+
+if user_choice == computer_choice:
+    print("TIE")
+
+elif user_choice == 'rock' and computer_choice == "paper":
+    print("THE WINNER IS PAPER")
+    print("The computer won, sorry!")
+
+elif user_choice == 'rock' and computer_choice == "scissors":
+    print("THE WINNER IS ROCK")
+    print("You Win!!  Yay you!")
+
+elif user_choice == 'paper' and computer_choice == "scissors":
+    print("THE WINNER IS SCISSORS")
+    print("The computer won, sorry!")
+
+elif user_choice == 'paper' and computer_choice == "rock":
+    print("THE WINNER IS PAPER")
+    print("You Win!!  Yay you!")
+
+elif user_choice == 'scissors' and computer_choice == "paper":
+    print("THE WINNER IS SCISSORS")
+    print("You Win!!  Yay you!")
+
+elif user_choice == 'scissors' and computer_choice == "rock":
+    print("THE WINNER IS ROCK")
+    print("The computer won, sorry!")
+
+
+
+
+
+
+
+
+
 
 
 
