7101d378b77f17e926b04966d31ccf5003a0ecd0
Rich Bubbico <rjb2182@stern.nyu.edu>
2019-06-05 19:08:53 -0400

    Validate input

--- game.py
+++ game.py
@@ -10,8 +10,16 @@ print("____________")
 print("YOU CHOSE:", user_choice)
 #  Validate inputs
 
+
+if user_choice in ["rock", "paper", "scissors"]:
+    print("VALID")
+else:
+    print("Invalid selection, please try again...")
+    exit()
+
 #  Generate computer selection
 
+print("Generating...")
 #  Determine the winner
 
 #  Display final outputs / outcomes
